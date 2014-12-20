02315cfff20a5e23cad28ac6c50d4775c3ad70c6
Eric Harrigan <harrharw42@gmail.com.com>
2014-12-20 15:26:54 -0500

    adding function details after fork

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Simple set of functions to: 
+## 1. create an "extended matrix" that holds functions to get/set the matrix value and the inverse matrix value
+## 2. calculate the inverse of a matrix and cache the value
 
-## Write a short comment describing this function
+## function:  makeCacheMatrix
+## Creates a "special matrix" with functions to get/set the matrix instance as well as to get/set the inverse of the matrix
+## Note:  it is assumed that the matrix passed in is an invertible square matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseMatrix <- NULL
+    set <- function(y) {
+        x <<- y
+        inverseMatrix <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(newInverse) inverseMatrix <<- newInverse
+    getInverse <- function() inverseMatrix
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## function cacheSolve
+## Accepts a matrix created via "makeCacheMatrix" and will do the following:
+## return the cached inverse matrix if available
+## calc the inverse of the matrix, cache it, and return the value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverseToReturn <- x$getInverse()
+    if(!is.null(inverseToReturn)) {
+        message("getting cached data")
+        return(inverseToReturn)
+    }
+    origMatrix <- x$get()
+    inverseToReturn <- solve(origMatrix)
+    x$setInverse(inverseToReturn)
+    inverseToReturn
 }
